ce252ee27b331af20c436616cd61fb0e3e77a5c3
Nicolas <nicolasrmattar@gmail.com>
2026-09-07 22:48:13 +0400

    MongoDB connection + model setup working

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+
 const app = express();
 const port = 3000;
+
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+
 
 app.get('/', (req, res) => {
   res.send('Note-taking app is running!');
