4cb0d2df0f94bf8e09eeac6780179ee741b2cce3
Nicolas <nicolasrmattar@gmail.com>
2026-09-07 16:35:38 -0400

    Added PUT and DELETE routes for notes

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -19,4 +19,20 @@ router.post('/', async (req, res) => {
 });
 
 
+// Updates an existing note by its ID
+router.put('/:id', async (req, res) => {
+    const updatedNote = await Note.findByIdAndUpdate(
+        req.params.id, 
+        req.body, 
+        { new: true }); 
+    res.json(updatedNote);
+});
+
+// Deletes a note by its ID
+router.delete('/:id', async (req, res) => {
+    await Note.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Note deleted successfully' });
+});
+
+
 module.exports = router;
